37598a8cd688a904dcf213fb87937ed73082e1fc
waseem <waseemmedhatg@gmail.com>
2020-11-24 13:11:15 +0200

    layout and theme

--- app.R
+++ app.R
@@ -1,17 +1,24 @@
 library(shiny)
+library(shinythemes)
 library(r2d3)
+library(shinyhelper)
 
 ui <- fluidPage(
+  theme = shinytheme('united'),
+  style = 'margin: 10px 20px;',
   
   div(
     class = 'page-header',
     h1('Visualizing Heterogeneity in Meta-Analyses'),
-    h3('Forest Plot with Heterogeneity Bands')
+    helper(
+      h3('Forest Plot with Heterogeneity Bands'),
+      content = 'interpretation',
+      size = 'l'
+    )
   ),
   
   sidebarLayout(
     sidebarPanel(
-      width = 3,
       radioButtons(
         'summary_measure',
         label = 'Choose summary measure:',
@@ -21,40 +28,20 @@ ui <- fluidPage(
       actionButton('visualize', 'Visualize')
     ),
     mainPanel(
-      width = 9,
       fluidRow(
-        column(
-          width = 10,
-          offset = 1,
-          div(
-            id ='forestContainer',
-            style = 'height: 400px;',
-            d3Output('d3Forest')
-          )
+        div(
+          id ='forestContainer',
+          style = 'height: 400px;',
+          d3Output('d3Forest')
         )
       )
     )
   ),
-  hr(),
-  
-  h3('How to interpret?'),
-  tags$ul(
-    tags$li(
-      'The bands augment the classic forest plot to provide a better visualization of heterogeneity of effect among studies.'
-    ),
-    tags$li(
-      'They '
-    ),
-    tags$li(
-      'How it works:',
-      tags$ul(
-        tags$li('Shades')
-      )
-    )
-  )
 )
 
 server <- function(input, output) {
+  observe_helpers()
+  
   metadata <- eventReactive(input$visualize, {
     readRDS(
       sprintf('data/metadata_%s.rds', input$summary_measure)
