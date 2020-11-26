32dacb64cc7ba8bb02c0d7b1330bd951299014f7
waseem <waseemmedhatg@gmail.com>
2020-11-26 16:04:14 +0200

    formatting

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(shinyhelper)
 
 ui <- fluidPage(
   theme = shinytheme('united'),
+  title = 'Visualizing Heterogeneity in Meta-Analyses',
 
   tags$style('
     body {
@@ -28,80 +29,54 @@ ui <- fluidPage(
     }
   '),
   
-  div(
-    class = 'page-header',
-    h1('Visualizing Heterogeneity in Meta-Analyses'),
-    helper(
-      h3('Forest Plot with Heterogeneity Bands'),
-      content = 'interpretation',
-      colour = 'teal',
-      buttonLabel = 'OK'
-    )
-  ),
+  div(class = 'page-header',
+      h1('Visualizing Heterogeneity in Meta-Analyses'),
+      helper(content = 'interpretation',
+             colour = 'teal',
+             buttonLabel = 'OK',
+             h3('Forest Plot with Heterogeneity Bands'))),
   
-  fluidRow(
-    column(
-      6, 
-      h4('Treatment groups'),
-      p('Intensive antihypertensive therapy (treatment) vs. standard of care (control)')
-    ),
-    column(
-      6,
-      h4('Outcome measure'),
-      p('Responder analysis - patients with controlled systolic blood pressure at 1 year (≤ 120 mmHg)'),
-    ),
-  ),
+  fluidRow(column(6, 
+                  h4('Treatment groups'),
+                  p('Intensive antihypertensive therapy (treatment)
+                    vs. standard of care (control)')),
+           column(6,
+                  h4('Outcome measure'),
+                  p('Responder analysis - patients with controlled
+                    systolic blood pressure at 1 year (≤ 120 mmHg)'))),
   
-  wellPanel(
-    style = 'margin: 20px auto;',
-    fluidRow(
-      id = 'control-panel',
-      column(
-        2,
-        radioButtons(
-          'summary_measure',
-          label = 'Choose summary measure:',
-          choices = list('Odds ratio' = 'or', 'Risk ratio' = 'rr')
-        )
-      ),
-      column(
-        5,
-        sliderInput(
-          'd3_width',
-          'Plot width:',
-          value = 900,
-          min = 650,
-          max = 1300,
-          step = 10,
-          post = ' px'
-        )
-      ),
-      column(
-        5,
-        sliderInput(
-          'd3_height',
-          'Plot height:',
-          value = 500,
-          min = 350,
-          step = 10,
-          max = 800,
-          post = ' px'
-        )
-      )
-    )
-  ),
+  wellPanel(style = 'margin: 20px auto;',
+            fluidRow(id = 'control-panel',
+                     column(2,
+                            radioButtons('summary_measure',
+                                         label = 'Choose summary measure:',
+                                         choices = list('Odds ratio' = 'or',
+                                                        'Risk ratio' = 'rr'))),
+                     column(5,
+                            sliderInput('d3_width',
+                                        'Plot width:',
+                                        value = 900,
+                                        min = 650,
+                                        max = 1300,
+                                        step = 10,
+                                        post = ' px')),
+                     column(5,
+                            sliderInput('d3_height',
+                                        'Plot height:',
+                                        value = 500,
+                                        min = 350,
+                                        step = 10,
+                                        max = 800,
+                                        post = ' px')))),
   
-  div(id ='forest-container', style = 'min-height: 350px;', d3Output('d3Forest')),
+  div(id ='forest-container',
+      style = 'min-height: 350px;',
+      d3Output('d3Forest')),
   
   hr(),
-  tags$footer(
-    'Built by Waseem Medhat for ',
-    a(
-      'Wonderful Wednesdays',
-      href = 'https://psiweb.org/sigs-special-interest-groups/visualisation/welcome-to-wonderful-wednesdays'
-    )
-  )
-)
+  tags$footer('Built by Waseem Medhat for ',
+              a('Wonderful Wednesdays',
+                href = 'https://psiweb.org/sigs-special-interest-groups/visualisation/welcome-to-wonderful-wednesdays')))
 
 server <- function(input, output) {
   observe_helpers()
@@ -110,7 +85,9 @@ server <- function(input, output) {
     metadata <- readRDS(
       sprintf('data/metadata_%s.rds', input$summary_measure)
     )    
+    
     removeUI('#d3Forest')
+    
     insertUI(
       selector = '#forest-container',
       where = 'afterBegin',
@@ -120,7 +97,6 @@ server <- function(input, output) {
     output$d3Forest <- renderD3({
       r2d3(metadata, script = 'src/forestWithBands.js')
     })
-    
   })
   
 }
